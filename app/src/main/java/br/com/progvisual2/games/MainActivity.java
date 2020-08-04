2ad4d4a77aba766ea543f872027c4f407e85b244
EDUARDO VIEIRA OLIVEIRA <eduardovieiraoliveiraa@gmail.com>
2020-08-04 12:26:18 -0300

    Implementações para chamada da API via Retrofit. Metodo de Banners realizado com sucesso. Utilizei retrofit, Gson e Picasso.
    Obtive algumas dificuldades, com alguns erros mas consegui solucionar.

--- app/src/main/java/br/com/progvisual2/games/MainActivity.java
+++ app/src/main/java/br/com/progvisual2/games/MainActivity.java
@@ -2,13 +2,10 @@ package br.com.progvisual2.games;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.content.Context;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
-import com.google.gson.Gson;
+
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -24,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     ImageView banner01;
     ImageView banner02;
     ImageView banner03;
+    private ArrayList<Banners> urls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         banner02 = findViewById(R.id.Banner02);
         banner03 = findViewById(R.id.Banner03);
 
+
+        ArrayList<Banners> urls = new ArrayList<>();
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(ServiceInterface.api_base)
                 .addConverterFactory(GsonConverterFactory.create())
@@ -54,9 +54,18 @@ public class MainActivity extends AppCompatActivity {
                     for(Banners a : t){
 
                         Log.i("eduardo",String.format("%s, %s: %s",a.id,a.image,a.url));
-                        Picasso.with(MainActivity.this).load(a.image).into(banner01);
 
+                        //ADCIONANDO AS URLS DOS BANNERS EM UM ARRAYLIST PARA DEPOIS INSERIR NOS IMAGEVIEW
+                        urls.add(a);
                     }
+
+                    //CARREGANDO OS BANNER NA ACTIVITY COM O PICASSO
+                        Banners b1 = urls.get(0);
+                        Banners b2 = urls.get(1);
+                        Banners b3 = urls.get(2);
+                        Picasso.with(MainActivity.this).load(b1.image).into(banner01);
+                        Picasso.with(MainActivity.this).load(b2.image).into(banner02);
+                        Picasso.with(MainActivity.this).load(b3.image).into(banner03);
                 }
             }
 
